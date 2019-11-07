051e3fd95034f8d9ce49bcf78545e10970dbf114
John Shin <jshin029@ucr.edu>
2019-11-06 23:03:25 -0800

    added brian

--- src/components/Judges.js
+++ src/components/Judges.js
@@ -13,19 +13,19 @@ class Judges extends Component {
             <h4 className="judgesHeader">ESTEEMED JUDGES</h4>
             <div style={{width: '16%', border: '1px solid #F7DE98', margin: 'auto', marginTop: '-4px', marginBottom: '12px'}}></div>
             <div className="row judgeRow">
-              <div className="col-4 judgeCol">
+              <div className="col xs={12} md={4} lg={4} xl={4} judgeCol">
                 <div className="craigImgContainer">
                 </div>
                 <h4 className="judgeImgHeader craig">Craig Schroeder</h4>
                 <p className="judgeImgSub">Assistant Professor in CSE Department</p>
               </div>
-              <div className="col-4 judgeCol">
+              <div className="col xs={12} md={4} lg={4} xl={4} judgeCol">
                 <div className="mariamImgContainer">
                 </div>
                 <h4 className="judgeImgHeader mariam">Mariam Salloum</h4>
                 <p className="judgeImgSub">Assistant Professor in CSE Department</p>
               </div>
-              <div className="col-4 judgeCol">
+              <div className="col xs={12} md={4} lg={4} xl={4} judgeCol">
                 <div className="lependuImgContainer">
                 </div>
                 <h4 className="judgeImgHeader paea">Paea LePendu</h4>
@@ -33,17 +33,23 @@ class Judges extends Component {
               </div>
             </div>
             <div className="row judgeRow">
-              <div className="col-4 judgeCol">
+              <div className="col xs={12} md={4} lg={4} xl={4} judgeCol">
                 <div className="papaImgContainer">
                 </div>
                 <h4 className="judgeImgHeader vagelis">Vagelis Papalexakis</h4>
                 <p className="judgeImgSub">Assistant Professor in CSE Department</p>
               </div>
-              <div className="col-4 judgeCol">
+              <div className="col xs={12} md={4} lg={4} xl={4} judgeCol">
                 <div className="jedImgContainer">
                 </div>
                 <h4 className="judgeImgHeader jed">Jed Schwendiman</h4>
                 <p className="judgeImgSub">Assistant Dean for Development, BCOE</p>
+              </div>
+              <div className="col xs={12} md={4} lg={4} xl={4} judgeCol">
+                <div className="brianImgContainer">
+                </div>
+                <h4 className="judgeImgHeader brian">Brian Crites</h4>
+                <p className="judgeImgSub">Assistant Professor in CSE Department</p>
               </div>
             </div>
           </div>
